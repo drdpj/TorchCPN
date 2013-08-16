24e7d95b189b0a960369fa1addcb6b74d090e0d0
Daniel Jameson <djameson@gmail.com>
2013-08-16 08:53:07 +0100

    More bits and bobs

---
+++ TorchCPN/src/net/umbriel/torch/AllocationMapSector.java
@@ -0,0 +1,10 @@
+package net.umbriel.torch;
+
+public class AllocationMapSector extends Sector {
+
+	public AllocationMapSector(int track, int side, int sector) {
+		super(track, side, sector);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ TorchCPN/src/net/umbriel/torch/DirectorySector.java
@@ -0,0 +1,10 @@
+package net.umbriel.torch;
+
+public class DirectorySector extends Sector {
+
+	public DirectorySector(int track, int side, int sector) {
+		super(track, side, sector);
+		// TODO Auto-generated constructor stub
+	}
+
+}
